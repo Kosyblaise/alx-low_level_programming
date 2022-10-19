9babc2ca2fcfc1857885a76eb63298848dcec325
Kosiso <kosyrex97@gmail.com>
2022-10-19 03:59:44 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,20 +1,35 @@
+#ifndef MAIN_H_INCLUDED
 
-/**
- * File:main.h
- * Author:Kosiso Uliah
- * Desc:header file containing functions declaration
- */
+#define MAIN_H_INCLUDED
+
+
+
+int _putchar(char c);
 
 void print_alphabet(void);
-void jack_bauer(void);
+
 void print_alphabet_x10(void);
-void times_table();
-void print_to_98(int);
-void print_times_table(int);
-int _putchar(char);
+
 int _islower(int c);
+
 int _isalpha(int c);
+
 int print_sign(int n);
-int _abs(int);
-int add(int n, int m);
+
+int _abs(int n);
+
 int print_last_digit(int n);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int a, int b);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+
+
+#endif
