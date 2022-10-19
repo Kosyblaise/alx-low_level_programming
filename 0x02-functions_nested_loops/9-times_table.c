da983129530dd47cb6e3861711ae0ea718eb207d
Kosiso <kosyrex97@gmail.com>
2022-10-19 15:12:57 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,6 +24,11 @@ void times_table(void)
 				if (b != 0)
 				{
 					_putchar(',');
+					_putchar((c % 10) + '0');
+				}
+				if (b < 9)
+				{
+					_putchar(',');
 					_putchar(' ');
 				}
 			}
